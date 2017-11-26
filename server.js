8376b0f1cd195e831b0380f8c45ec71cb32622d5
Brett Fuller <bfuller123@gmail.com>
2017-11-25 22:27:46 -0600

    scraper works

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bp = require('body-parser');
+const cheerio = require("cheerio");
+const request = require("request");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +10,19 @@ app.use(express.static('view'));
 
 app.get('/', function(req, res){
   res.sendFile('/index.html')
+})
+
+app.get('/scrape', function(req, res){
+  request("https://www.nytimes.com", function(error, response, html){
+    let $ = cheerio.load(html);
+    let results = {};
+    $(".story-heading").each(function(i, element){
+      let headline = $(element).children().text();
+      let link = $(element).children().attr("href");
+      results[headline] = link;
+    })
+    res.send(results);
+  })
 })
 
 app.listen(PORT, function(){
